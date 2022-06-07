1946bc904d5b13d55065f208562cb5650d73db6c
Kolyganov Ivan <adamjensen2302@gmail.com>
2022-06-07 23:18:22 +0500

    getResumesForManager
    получили целое резюме для менеджера

--- frontend/src/Components/Manager/ResumeSearch.tsx
+++ frontend/src/Components/Manager/ResumeSearch.tsx
@@ -7,44 +7,78 @@ function ResumeSearch() {
     const [state, setState] = useState({
         resumes:[{
             id: 0,
-            email: "user@example.com",
-            full_name: "string",
-            department: {
+            applicant: {
                 id: 0,
-                name: "string"
-            }
+                email: "user@example.com",
+                full_name: "string",
+                department: {
+                    id: 0,
+                    name: "string"
+                }
+            },
+            current_position: "string",
+            desired_position: "string",
+            skills: [
+                "string"
+            ],
+            experience: 0,
+            bio: "string"
         }]
     });
 
     useEffect(() => {
-        getApplicantsForManager()
+        getResumesForManager()
     }, []);
 
-    async function getApplicantsForManager() {
+    async function getResumesForManager() {
         let body = {
             jsonrpc: "2.0",
             id: 0,
-            method: "get_applicants_for_manager",
+            method: "get_resumes_for_manager",
             params: {}
         }
 
-        let response = await fetch('http://localhost:8000/api/v1/web/jsonrpc/get_applicants_for_manager', {
+        let response = await fetch('http://localhost:8000/api/v1/web/jsonrpc/get_resumes_for_manager', {
             method: 'POST',
             headers: {'Authorization': `Bearer ${localStorage.getItem('token')}`},
             body: JSON.stringify(body)
         });
         response.json().then(
             res => {
-                setState({resumes: res.result.items})}
+                setState({resumes: res.result.items})
+                console.log(res)
+            }
         )
     }
+    
+    
+    // async function getApplicantsForManager() {
+    //     let body = {
+    //         jsonrpc: "2.0",
+    //         id: 0,
+    //         method: "get_applicants_for_manager",
+    //         params: {}
+    //     }
+    //
+    //     let response = await fetch('http://localhost:8000/api/v1/web/jsonrpc/get_applicants_for_manager', {
+    //         method: 'POST',
+    //         headers: {'Authorization': `Bearer ${localStorage.getItem('token')}`},
+    //         body: JSON.stringify(body)
+    //     });
+    //     response.json().then(
+    //         res => {
+    //             setState({resumes: res.result.items})}
+    //     )
+    // }
 
     return (
         <div className={"general_background"}>
             {state.resumes.map(resume => {
                 return (
-                    <SearchResumeCard email={resume.email} full_name={resume.full_name}
-                                      department={resume.department.name}/>
+                    <SearchResumeCard applicant={resume.applicant} current_position={resume.current_position}
+                                      desired_position={resume.desired_position} skills={resume.skills}
+                                      experience={resume.experience} bio={resume.bio}
+                    />
                 )
             })}
         </div>
