b9927bbc59ab5104f225627a6f8aa27168c51503
Михалев Кирилл <mihalov.k@mail.ru>
2022-06-08 21:25:30 +0500

    Bug Fixes #2

--- frontend/src/Components/Manager/Cards/SearchResumeCard.tsx
+++ frontend/src/Components/Manager/Cards/SearchResumeCard.tsx
@@ -11,16 +11,15 @@ function SearchResumeCard(props:any) {
             <p>Департамент: {props.applicant.department.name}</p>
             <p className="resume-text">Текущая позиция: {props.current_position}</p>
             <p className="resume-text">Желаемая позиция: {props.desired_position}</p>
-            <p>Скил позиция</p>
+            <p>Навыки:</p>
             {props.skills.map((skill:string) => {
                 return (
                     <p>- {skill}</p>
                 )
             })}
-            <p className="resume-text">Опытная позиция {props.experience} (год/лет)</p>
-            <p>Биографическая позиция</p>
+            <p className="resume-text">Опыт разработки: {props.experience} (год/лет)</p>
+            <p>Резюме:</p>
             <p className="resume-text">{props.bio}</p>
-            <button className="btn btn-success mt-3 submit btn-width">Добавить в избранное</button>
         </div>
     )
 }
